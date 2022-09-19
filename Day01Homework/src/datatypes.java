c248a27ec895f913ff06b8cb94cdea8dee84e715
omerfulin <omerfulin@gmail.com>
2022-09-19 15:16:35 +0300

    Day01 Homework

---
+++ Day01Homework/src/datatypes.java
@@ -0,0 +1,12 @@
+
+public class datatypes {
+
+	public static void main(String[] args) {
+		int sayı = 129;
+		double sayi1 = 15.9;
+		String karakter = "Merhaba";
+		boolean dogruMu = false;
+		System.out.println(karakter);
+	}
+
+}
---
+++ Day01Homework/src/variables.java
@@ -0,0 +1,12 @@
+
+public class variables {
+
+	public static void main(String[] args) {
+		int öğrencisayısı = 12;
+		String mesaj = "Öğrenci Sayısı : "; 
+		System.out.println("Öğrenci Sayım : " + öğrencisayısı);
+		System.out.println(mesaj + öğrencisayısı);
+
+	}
+
+}
