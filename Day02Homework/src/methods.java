bf08eede332610df73cdb0fba854b516cf1b531b
omerfulin <omerfulin@gmail.com>
2022-09-23 02:42:16 +0300

    Day 02 Homework

---
+++ Day02Homework/src/methods.java
@@ -0,0 +1,34 @@
+public class methods {
+
+	public static void main(String[] args) {
+		sayiBulmaca();
+		mesajVer();
+	}	
+
+	public static void sayiBulmaca() {
+		int[] sayilar = { 1, 2, 5, 7, 9, 0 };
+
+		int aranacak = 6;
+		boolean varMi = false;
+
+		for (int sayi : sayilar) {
+			if (sayi == aranacak) {
+				varMi = true;
+				break;
+			}
+		}
+		
+		String mesaj ="";
+		if (varMi) {	
+			mesaj = "Sayı mevcuttur: " + aranacak;
+			mesajVer(mesaj);
+		} else {
+			mesajVer("Sayı mevcuttur Değildir : " + aranacak);
+		}
+
+	}	
+	public static void mesajVer(String mesaj) {
+		System.out.println(mesaj);
+		
+	}
+}	
