c08c01efdc4c51dc925ddcd7ec5351ebde028069
omerfulin <omerfulin@gmail.com>
2022-10-05 16:36:22 +0300

    Day 02 Homework

--- Day02Homework/src/methods2.java
+++ Day02Homework/src/methods2.java
@@ -7,6 +7,8 @@ public class methods2 {
 		System.out.println(yeniMesaj);
 		int sayi = topla(15,7);
 		System.out.println(sayi);
+		int toplam = topla2(10,20,3,4,5);
+		System.out.println(toplam);
 	}	
 	
 	public static void ekle() {
@@ -21,12 +23,20 @@ public class methods2 {
 	
 	public static void guncelle() {
 		System.out.println("Güncelledi");
-		
-		
+				
 	}
 	
 	public static int topla(int sayi1,int sayi2) {
 		return sayi1+sayi2;
+	}
+	
+	public static int topla2(int... sayilar) {  // 3 Nokta anlamı variables Arguments demektir.
+		int toplam  = 0;	
+		for (int sayi:sayilar) {
+			toplam += sayi;
+		}
+		return toplam;
+		
 	}
 	
 	public static String sehirVer() {
