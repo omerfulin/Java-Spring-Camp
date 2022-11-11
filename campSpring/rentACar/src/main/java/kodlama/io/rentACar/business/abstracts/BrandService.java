d576fa1a688c2142170edc31aade0c29d2b3b9d1
omerfulin <omerfulin@gmail.com>
2022-11-11 16:08:56 +0300

    Java Camp Day 04

---
+++ campSpring/rentACar/src/main/java/kodlama/io/rentACar/business/abstracts/BrandService.java
@@ -0,0 +1,9 @@
+package kodlama.io.rentACar.business.abstracts;
+
+import java.util.List;
+
+import kodlama.io.rentACar.entites.concretes.Brand;
+
+public interface BrandService {
+	List<Brand> getAll();
+}
